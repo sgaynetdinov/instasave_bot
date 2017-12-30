305d8a01d4a3d1e1c482450e30a883ed2cf27c53
Sergey G <s.gaynetdinov@gmail.com>
2017-12-31 03:19:11 +0500

    Delete `opbeat`

--- bot/wsgi.py
+++ bot/wsgi.py
@@ -1,23 +1,10 @@
 import falcon
 
-from opbeat import Client
-from opbeat.middleware import Opbeat
-
-from . import vk_bot, config
-from .middleware import JSONMiddleware, SecretKeyMiddleware, CheckGroupMiddleware, ConfirmationMiddleware, OpbeatAPMMiddleware
-
-
-client = Client(
-    organization_id=config.OPBEAT_ORGANIZATION_ID,
-    app_id=config.OPBEAT_APP_ID,
-    secret_token=config.OPBEAT_SECRET_TOKEN,
-    DEBUG=False,
-    TIMEOUT=5
-)
+from . import vk_bot
+from .middleware import JSONMiddleware, SecretKeyMiddleware, CheckGroupMiddleware, ConfirmationMiddleware
 
 
 application = start = falcon.API(middleware=[
-    OpbeatAPMMiddleware(client),
     JSONMiddleware(),
     SecretKeyMiddleware(),
     CheckGroupMiddleware(),
@@ -25,5 +12,3 @@ application = start = falcon.API(middleware=[
 ])
 
 application.add_route('/', vk_bot.Bot())
-
-application = Opbeat(application, client)
