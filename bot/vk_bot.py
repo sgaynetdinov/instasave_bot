6d9b0918b7f8813dd89d667f0f4f11cb6f63a866
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2018-10-13 19:26:49 +0500

    Replace `ThreadPoolExecutor` => `Process`

--- bot/vk_bot.py
+++ bot/vk_bot.py
@@ -1,9 +1,9 @@
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Process
 
 import vk
 
 from .config import VK_GROUP_ID, VK_GROUP_TOKEN
-from .instagram import is_instagram_link, get_instagram_photos, InstagramError
+from .instagram import InstagramError, get_instagram_photos, is_instagram_link
 
 api = vk.Api(VK_GROUP_TOKEN)
 group = api.get_group(VK_GROUP_ID)
@@ -15,31 +15,31 @@ class Bot:
         data = req.context['data']
 
         if "message_new" == data.get("type"):
-            with ThreadPoolExecutor(max_workers=1) as executor:
-                executor.submit(self.handler_new_message, data)
+            Process(target=handler_new_message, args=(data,))
 
-    def handler_new_message(self, data):
-        message_text = data['object']['body']
-        user_id = data['object']['user_id']
 
-        user = api.get_user(user_id)
+def handler_new_message(data):
+    message_text = data['object']['body']
+    user_id = data['object']['user_id']
 
-        if user not in group:
-            group.messages_set_typing(user)
-            group.send_messages(user_id, message='Пожалуйста вступите в сообщество https://vk.com/instasave_bot :v:')
+    user = api.get_user(user_id)
 
-        self.send_message(message_text, user)
-
-    def send_message(self, instagram_link, user):
+    if user not in group:
         group.messages_set_typing(user)
+        group.send_messages(user_id, message='Пожалуйста вступите в сообщество https://vk.com/instasave_bot :v:')
 
-        if not is_instagram_link(instagram_link):
-            group.send_messages(user.id, message='Отправьте пожалуйста ссылку на фото из instagram.com')
-            return None
+    send_message(message_text, user)
 
-        try:
-            for instagram_photo in get_instagram_photos(instagram_link):
-                group.messages_set_typing(user)
-                group.send_messages(user.id, image_files=[instagram_photo])
-        except InstagramError:
-            group.send_messages(user.id, message='Не могу найти, возможно фото/видео доступно только для подписчиков (приватный аккаунт)')
+def send_message(instagram_link, user):
+    group.messages_set_typing(user)
+
+    if not is_instagram_link(instagram_link):
+        group.send_messages(user.id, message='Отправьте пожалуйста ссылку на фото из instagram.com')
+        return None
+
+    try:
+        for instagram_photo in get_instagram_photos(instagram_link):
+            group.messages_set_typing(user)
+            group.send_messages(user.id, image_files=[instagram_photo])
+    except InstagramError:
+        group.send_messages(user.id, message='Не могу найти, возможно фото/видео доступно только для подписчиков (приватный аккаунт)')
