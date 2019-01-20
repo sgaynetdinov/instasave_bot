69beb8e351b2dd2ebd5caf4e92102da7d617f3e7
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-01-21 00:34:25 +0500

    Refactoring: delete `send_message`

--- bot/vk_bot.py
+++ bot/vk_bot.py
@@ -24,27 +24,21 @@ class Bot:
 
 
 def handler_new_message(data):
-    message_text = data['object']['body']
+    link = data['object']['body']
     user_id = data['object']['user_id']
 
-    if not Instagram.is_instagram_link(message_text):
-        group.send_messages(user_id, message='Отправьте пожалуйста ссылку на фото из Instagram')
-        return None
-
-    send_message(message_text, user_id)
-
-
-def send_message(instagram_link, user_id):
     group.messages_set_typing(user_id)
 
-    try:
-        instagram = Instagram.from_url(instagram_link)
-    except Instagram404Error:
-        group.send_messages(user_id, message='Не могу найти, возможно фото/видео доступно только для подписчиков (приватный аккаунт)')
+    if not Instagram.is_instagram_link(link):
+        group.send_messages(user_id, message='Отправьте пожалуйста ссылку на фото из Instagram')
         return
 
-    group.send_messages(user_id, message=instagram.get_text())
-
-    for url in instagram.get_photos_url():
-        group.messages_set_typing(user_id)
-        group.send_messages(user_id, image_files=[urlopen(url)])
+    try:
+        instagram = Instagram.from_url(link)
+    except Instagram404Error:
+        group.send_messages(user_id, message='Не могу найти, возможно фото/видео доступно только для подписчиков (приватный аккаунт)')
+    else:
+        group.send_messages(user_id, message=instagram.get_text())
+        for url in instagram.get_photos_url():
+            group.messages_set_typing(user_id)
+            group.send_messages(user_id, image_files=[urlopen(url)])
