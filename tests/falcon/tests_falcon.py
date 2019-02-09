fb876028127876502357b0bc3a7af875fd0c7949
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-02-10 01:52:46 +0500

    Delete 'FalconBaseTestCase'

--- tests/falcon/tests_falcon.py
+++ tests/falcon/tests_falcon.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ['GROUP_ID'] = '1'
-        
+
 from urllib.parse import urlencode
 
 import falcon
@@ -10,28 +10,21 @@ from falcon import testing
 from bot.middleware import JSONMiddleware, SecretKeyMiddleware, CheckGroupMiddleware, ConfirmationMiddleware
 
 
-class FalconBaseTestCase(testing.TestCase):
+class JSONMiddlewareTestCase(testing.TestCase):
     def setUp(self):
         super().setUp()
 
-        self.app = falcon.API(middleware=[
-            JSONMiddleware(),
-            SecretKeyMiddleware(),
-            CheckGroupMiddleware(),
-            ConfirmationMiddleware()
-        ])
+        self.app = falcon.API(middleware=JSONMiddleware())
         self.app.add_route('/', testing.SimpleTestResource())
 
-class JSONMiddlewareTestCase(FalconBaseTestCase):
     def test_empty_request(self):
-       got = self.simulate_post('/') 
+       got = self.simulate_post('/')
 
        self.assertEqual(got.json['title'], 'Not empty')
        self.assertEqual(got.status, falcon.HTTP_400)
 
     def test_not_valid_json(self):
        got = self.simulate_post('/', body=b'{a: 3}')
-       
+
        self.assertEqual(got.json['title'], 'Not valid JSON')
        self.assertEqual(got.status, falcon.HTTP_400)
-
