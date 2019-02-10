3bcfe4a4003e4e8e491f195186ba3f5fbd7ec70f
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-02-10 21:09:51 +0500

    Add tests for 'SecretKeyMiddleware'

---
+++ tests/falcon/tests_middleware_secret_key.py
@@ -0,0 +1,31 @@
+import os
+
+os.environ['SECRET_KEY'] = 'SECRET'
+
+import falcon
+from falcon import testing
+
+from bot.middleware import JSONMiddleware, SecretKeyMiddleware 
+
+
+class SecretKeyMiddlewareTestCase(testing.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.app = falcon.API(middleware=[JSONMiddleware(), SecretKeyMiddleware()])
+        self.app.add_route('/', testing.SimpleTestResource(json={'title': 'Success test'}))
+
+        self.data = b'{"secret": "SECRET", "type": "message_new"}'
+
+    def test_valid_secret(self):
+        got = self.simulate_post('/', body=self.data)
+
+        self.assertEqual(got.json['title'], 'Success test')
+        self.assertEqual(got.status, falcon.HTTP_200)
+
+    def test_invalid_secret(self):
+        got = self.simulate_post('/', body=b'{"secret": "INVALID_SECRET", "type": "message_new"}')
+
+        self.assertEqual(got.json['title'], 'Invalid request')
+        self.assertEqual(got.status, falcon.HTTP_400)
+
