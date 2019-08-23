1f26efcc81fd7e7b0b4e2109462b28bb2fff22ab
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-08-23 12:47:35 +0500

    Add test handler new message

--- tests/falcon/tests_confirmation.py
+++ tests/falcon/tests_confirmation.py
@@ -1,9 +1,11 @@
 import os
+from unittest.mock import patch
 
 import falcon
 from falcon import testing
 
 from bot.middleware import JSONMiddleware
+from bot.vk_bot import Process
 from bot.wsgi import application
 
 os.environ['CONFIRMATION_KEY'] = 'response confirmation'
@@ -16,6 +18,12 @@ class ConfirmationTestCase(testing.TestCase):
         super().setUp()
 
         self.app = application
+
+    @patch('bot.vk_bot.Process.start')
+    def test_type_message_new(self, mock):
+        got = self.simulate_post('/', body=b'{"type": "message_new", "object": {"body": 100500, "user_id": 200600}, "secret": "SECRET", "group_id": 100500}')
+
+        self.assertEqual(mock.call_count, 1)
 
     def test_type_not_confirmation(self):
         got = self.simulate_post('/', body=b'{"type": "message_reply", "group_id": 100500, "secret": "SECRET"}')
