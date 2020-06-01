19576aa43f067e312c26a347cac32c0f69f26f7c
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2020-06-01 22:48:28 +0500

    Fix sort import

--- bot/wsgi.py
+++ bot/wsgi.py
@@ -8,7 +8,6 @@ from .middleware import (CheckGroupMiddleware, JSONMiddleware,
                          SecretKeyMiddleware)
 from .vk_bot import Bot
 
-
 sentry_dsn = os.environ.get('SENTRY_DSN')
 if sentry_dsn:
     sentry_sdk.init(
