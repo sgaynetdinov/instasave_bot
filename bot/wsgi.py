d7e648f482e1ed428b514a48511c286d1b90edf8
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2020-06-01 22:46:32 +0500

    Add 'sentry' for logging error

--- bot/wsgi.py
+++ bot/wsgi.py
@@ -1,8 +1,20 @@
+import os
+
 import falcon
+import sentry_sdk
+from sentry_sdk.integrations.falcon import FalconIntegration
 
 from .middleware import (CheckGroupMiddleware, JSONMiddleware,
                          SecretKeyMiddleware)
 from .vk_bot import Bot
+
+
+sentry_dsn = os.environ.get('SENTRY_DSN')
+if sentry_dsn:
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[FalconIntegration()]
+    )
 
 application = falcon.API(middleware=[
     JSONMiddleware(),
