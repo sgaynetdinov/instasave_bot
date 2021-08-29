a3fe47bdbac56fb9c2bf0aa9ed4075bfb267d411
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2021-08-30 01:26:29 +0500

    Refactoring

--- bot/vk_bot.py
+++ bot/vk_bot.py
@@ -8,6 +8,7 @@ from .instagram import Instagram, Instagram404Error, InstagramLinkError
 
 VK_GROUP_TOKEN = os.environ.get('GROUP_TOKEN')
 VK_GROUP_ID = os.environ.get('GROUP_ID')
+MESSAGE_IF_NOT_MEMBERS = os.environ.get("MESSAGE_IF_NOT_MEMBERS")
 
 api = vk.Api(VK_GROUP_TOKEN)
 group = api.get_group(VK_GROUP_ID)
@@ -30,9 +31,6 @@ class Bot:
     def handler_new_message(self, link, user_id):
         group.messages_set_typing(user_id)
 
-        MESSAGE_IF_NOT_MEMBERS = os.environ.get("MESSAGE_IF_NOT_MEMBERS")
-        user_not_member = MESSAGE_IF_NOT_MEMBERS and user_id not in group
-
         try:
             instagram = Instagram.from_url(link)
         except Instagram404Error:
@@ -40,18 +38,25 @@ class Bot:
         except InstagramLinkError:
             group.send_messages(user_id, message='Отправьте пожалуйста ссылку на фото из Instagram')
         else:
-            text = instagram.get_text()
-            if text:
-                group.send_messages(user_id, message=text)
+            user_is_member = user_id in group
+            self.send_message(instagram, user_id, user_is_member)
 
-            urls = instagram.get_photos_and_video_url()
-            for url in urls: 
+    def send_message(self, instagram, user_id, user_is_member):
+        text = instagram.get_text()
+        if text:
+            group.send_messages(user_id, message=text)
+
+        urls = instagram.get_photos_and_video_url()
+        if not user_is_member and MESSAGE_IF_NOT_MEMBERS:
+            urls = urls[:1]
+
+        for url in urls:
+            group.messages_set_typing(user_id)
+            if '.mp4' in url:
+                group.send_messages(user_id, message=url)
+            else:
+                group.send_messages(user_id, image_files=[urlopen(url)])
+            if not user_is_member and MESSAGE_IF_NOT_MEMBERS:
                 group.messages_set_typing(user_id)
-                if '.mp4' in url:
-                    group.send_messages(user_id, message=url)
-                else:
-                    group.send_messages(user_id, image_files=[urlopen(url)])
-                if user_not_member and len(urls) > 1:
-                    group.messages_set_typing(user_id)
-                    group.send_messages(user_id, message=MESSAGE_IF_NOT_MEMBERS)
-                    continue
+                group.send_messages(user_id, message=MESSAGE_IF_NOT_MEMBERS)
+
